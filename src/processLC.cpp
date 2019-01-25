439b00ec54140ed1cb52ed84699c3107294e461a
Joe Hollowed <jphollowed@gmail.com>
2019-01-25 20:41:48 +0000

    removing debug statements

--- src/processLC.cpp
+++ src/processLC.cpp
@@ -1075,16 +1075,7 @@ void processLC(string dir_name, vector<string> out_dirs, vector<string> step_str
         
         // time sort 
         MPI_Barrier(MPI_COMM_WORLD);
-        //cout << "for rank " << myrank << ": " << " Np = " << Np << " :: len(recv) = " << recv_particles_pos.size() << endl;
         start = MPI_Wtime();
-        //clock_t thisRank_argstart = clock();
-        //clock_t thisRank_argend;
-
-        //if(myrank == 41){
-        //    ofstream fout("data2.dat", ios::out | ios::binary);
-        //    fout.write((char*)&recv_particles_pos[0], recv_particles_pos.size() * sizeof(particle_pos));
-        //    fout.close();
-        //}
         
         // arg sort to map new ordering to recv_particles_vel 
         vector<int> theta_argSort(Np);
@@ -1093,27 +1084,8 @@ void processLC(string dir_name, vector<string> out_dirs, vector<string> step_str
         stable_sort(theta_argSort.begin(), theta_argSort.end(), 
              [&](int n, int m){return recv_particles_pos[n].theta < recv_particles_pos[m].theta;} );
         
-        //thisRank_argend = clock();
-        //clock_t thisRank_argtime = thisRank_argend - thisRank_argstart;
-        //double thisRank_argsecs = thisRank_argtime / (double) CLOCKS_PER_SEC;
-        //cout << "rank " << myrank << " finished  arg in " << thisRank_argsecs << " s" << endl;
-
-        //MPI_Barrier(MPI_COMM_WORLD);
-        //stop = MPI_Wtime(); 
-        //duration = stop - start;
-        //if(myrank == 0 and timeit == true){ cout << "Particle argsort time: " << duration << " s" << endl; }
-        //start = MPI_Wtime();
-         
-        //clock_t thisRank_start = clock();
-        //clock_t thisRank_end;
-        
         // sort recv_particles_pos such that theta is in ascending order
         stable_sort(recv_particles_pos.begin(), recv_particles_pos.end(), comp_by_theta);
-        
-        //thisRank_end = clock();
-        //clock_t thisRank_time = thisRank_end - thisRank_start;
-        //double thisRank_secs = thisRank_time / (double) CLOCKS_PER_SEC;
-        //cout << "rank " << myrank << " finished  in " << thisRank_secs << " s" << endl;
         
         MPI_Barrier(MPI_COMM_WORLD);
         stop = MPI_Wtime(); 
@@ -1121,48 +1093,7 @@ void processLC(string dir_name, vector<string> out_dirs, vector<string> step_str
         if(myrank == 0 and timeit == true){ cout << "Particle sort time: " << duration << " s" << endl; }
         sort_times.push_back(duration);
         
-        /*
-        // check load balancing (all ranks should have taken more or less the same amount of time here)
-        clock_t thisRank_argtime = thisRank_argend - thisRank_argstart;
-        double thisRank_argsecs = thisRank_argtime / (double) CLOCKS_PER_SEC;
-        vector<double> allRank_argsecs(numranks);
-        clock_t thisRank_time = thisRank_end - thisRank_start;
-        double thisRank_secs = thisRank_time / (double) CLOCKS_PER_SEC;
-        vector<double> allRank_secs(numranks);
-        
-        MPI_Allgather(&thisRank_argsecs, 1, MPI_DOUBLE, 
-                      &allRank_argsecs[0], 1, MPI_DOUBLE, MPI_COMM_WORLD);
-        MPI_Allgather(&thisRank_secs, 1, MPI_DOUBLE, 
-                      &allRank_secs[0], 1, MPI_DOUBLE, MPI_COMM_WORLD);
 
-        if(myrank == 0){
-            double min_argcompTime = 9999;
-            double max_argcompTime = 0;
-            double mean_argcompTime;
-            double min_compTime = 9999;
-            double max_compTime = 0;
-            double mean_compTime;
-
-            for(int cc = 0; cc < numranks; ++cc){
-                if(allRank_argsecs[cc] < min_argcompTime){ min_argcompTime = allRank_argsecs[cc];}
-                if(allRank_argsecs[cc] > max_argcompTime){ max_argcompTime = allRank_argsecs[cc];}
-                if(allRank_secs[cc] < min_compTime){ min_compTime = allRank_secs[cc];}
-                if(allRank_secs[cc] > max_compTime){ max_compTime = allRank_secs[cc];}
-            }
-
-            mean_argcompTime = accumulate(allRank_argsecs.begin(), allRank_argsecs.end(), 0.0) / allRank_argsecs.size();
-            mean_compTime = accumulate(allRank_secs.begin(), allRank_secs.end(), 0.0) / allRank_secs.size();
-        
-            if(myrank == 0){
-                cout << "[min, mean, max] sort rank argsort times: [" << min_argcompTime << ", "
-                                                                   << mean_argcompTime << ", "
-                                                                   << max_argcompTime << "]" << endl;
-                cout << "[min, mean, max] sort rank sort times: [" << min_compTime << ", "
-                                                                   << mean_compTime << ", "
-                                                                   << max_compTime << "]" << endl;
-            }
-        }
-        */
         ///////////////////////////////////////////////////////////////
         //
         //                 Loop over all target halos
